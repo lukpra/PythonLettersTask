80fda8e0d6a970fa0a78aed175a145a71bdef62d
Łukasz Prażyński <praza99+git@gmail.com>
2016-06-21 23:34:13 +0200

    Staging progress

--- task.py
+++ task.py
@@ -17,27 +17,34 @@ def rewrite(argv):
     counter = 1
     l_counter =0
 
-    with open(argv[2]) as pant:
+    with open(argv[2], encoding='utf-8') as pant:
         print("Pant is opend")
         for char in input_file:
-            not_in_dict = False
+            print(output_string)
+            not_in_dict = True
             print("Chr: " + char + " index:" + str(l_counter))
             l_counter+=1
-            if char == " ":  # check for empty letter
+            if char == " " or char == " ":  # check for empty letter
                 output_string += char
                 output_ids.append(0)
             else:
                 if char.lower() in letters_dict:
                     try:
+                        print("Trying "+char+" lower: "+char.lower())
                         arr = letters_dict[char.lower()].popleft()
                         output_string += arr[0]
                         output_ids.append(arr[1])
+                        not_in_dict = False
+                        print("found in dict")
                     except:
                         # Letter in dictionary is exhausted
                         not_in_dict = True
                 if not_in_dict:
+                    print("not in dict")
                     while True:
-                        counter += counter
+                        counter += 1
+                        if counter < 250:
+                            print("Counter is: "+ str(counter) + " looking for: "+char)
                         pant_char = pant.read(1)
                         if not pant_char:
                             sys.exit(1)
@@ -54,6 +61,7 @@ def rewrite(argv):
         # input String can be rewritten
         # todo: generate output
         print(output_ids)
+        print(output_string)
 
 if __name__ == "__main__":
     sys.exit(rewrite(sys.argv))
